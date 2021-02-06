8d743dc0c4e28b2c6310ebb4194d7b872a8ec78b
David DeCorso <davidadecorso@gmail.com>
2021-02-05 20:01:50 -0500

    app framework first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.sass';
+import React from 'react';
+import Nav from './components/Nav';
+import Tabs from './components/Tabs';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      
+      <Tabs />
+    </div>
+  );
+}
+
+export default App;

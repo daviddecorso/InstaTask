47860481f39143813ad90b52bb220176e0cb7b34
David DeCorso <davidadecorso@gmail.com>
2021-02-19 01:07:19 -0500

    Created optimized view for mobile

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,18 +1,46 @@
-/* Disabling a linter warning that will be irrelevant
-    when a proper href value is provdided. */
+/* Disabling a linter warning that is caused by the CSS
+ * framework and can't be fixed without undue effort. */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
-const Tabs = () => {
+const Tabs = ({ activeTab, setTab, setTodoView, setCalView }) => {
+  // Changes to the todo view if todo tab is clicked
+  const onClickTodoTab = () => {
+    setTodoView(true);
+    setCalView(false);
+    setTab("todo");
+  };
+
+  // Changes to the calendar view if the calendar tab is clicked
+  const onClickCalTab = () => {
+    setCalView(true);
+    setTodoView(false);
+    setTab("cal");
+  };
+
   return (
     <div>
       <div className="tabs is-fullwidth is-medium">
         <ul>
-          <li className="is-active">
-            <a>To-Do</a>
-          </li>
-          <li>
-            <a href="">Calendar</a>
-          </li>
+          {activeTab === "todo" && (
+            <li className="is-active">
+              <a onClick={onClickTodoTab}>To-Do</a>
+            </li>
+          )}
+          {activeTab === "cal" && (
+            <li>
+              <a onClick={onClickTodoTab}>To-Do</a>
+            </li>
+          )}
+          {activeTab === "todo" && (
+            <li>
+              <a onClick={onClickCalTab}>Calendar</a>
+            </li>
+          )}
+          {activeTab === "cal" && (
+            <li className="is-active">
+              <a onClick={onClickCalTab}>Calendar</a>
+            </li>
+          )}
         </ul>
       </div>
     </div>

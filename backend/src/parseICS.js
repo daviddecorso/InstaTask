2894080d5b43be4082e110971c824b4193ef14a2
lucasrossell <lrossell517@gmail.com>
2021-02-21 16:24:20 -0500

    Made parseICS.js prettier

--- backend/src/parseICS.js
+++ backend/src/parseICS.js
@@ -6,29 +6,29 @@
  * @param {String} link
  */
 async function parseICS(link) {
-    const ical = require('node-ical');
-    var eventList = [];
-    let events = await ical.async.fromURL(link);
-    for(let d in events) {
-        if (events.hasOwnProperty(d)) {
-            if (events[d].type == 'VEVENT') {
-                eventObj = {
-                    dtstamp:events[d].dtstamp,
-                    uid:events[d].uid,
-                    dtstart:events[d].start,
-                    dtend:events[d].end,
-                    desc:events[d].description,
-                    location:events[d].location,
-                    sequence:events[d].sequence,
-                    summary:events[d].summary,
-                    complete:events[d].completed,
-                    hasAlerts:events[d].hasAlerts,
-                    alerts:events[d].alerts,
-                };
-                eventList.push(eventObj);
-            }
-        }
+  const ical = require("node-ical");
+  var eventList = [];
+  let events = await ical.async.fromURL(link);
+  for (let d in events) {
+    if (events.hasOwnProperty(d)) {
+      if (events[d].type == "VEVENT") {
+        eventObj = {
+          dtstamp: events[d].dtstamp,
+          uid: events[d].uid,
+          dtstart: events[d].start,
+          dtend: events[d].end,
+          desc: events[d].description,
+          location: events[d].location,
+          sequence: events[d].sequence,
+          summary: events[d].summary,
+          complete: events[d].completed,
+          hasAlerts: events[d].hasAlerts,
+          alerts: events[d].alerts,
+        };
+        eventList.push(eventObj);
+      }
     }
-    return eventList;
+  }
+  return eventList;
 }
-module.exports = {parseICS};
+module.exports = { parseICS };

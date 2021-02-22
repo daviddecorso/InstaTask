c8554c07b9208f43c3f7836982ca6171d275d868
David DeCorso <davidadecorso@gmail.com>
2021-02-22 18:20:07 -0500

    commented trimSummary

--- backend/src/trimSummary.js
+++ backend/src/trimSummary.js
@@ -1,8 +1,13 @@
+/**
+ * Takes the summary field from a canvas calendar event and returns just the class code
+ * @param {String} summary
+ */
 function trimSummary(summary) {
   // The class code is contained in brackets at the end of the summary string.
   summaryArr = summary.split("[");
   classString = summaryArr[summaryArr.length - 1];
 
+  // Regex that matches to UCF course codes
   regexArr = classString.match(/[A-Z][A-Z][A-Z]\d\d\d\d[A-Z]?/g);
 
   if (regexArr != null) {

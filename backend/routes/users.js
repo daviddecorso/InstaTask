45dd0516463b8829efbe4122cefc35f6040bc776
lucasrossell <lrossell517@gmail.com>
2021-02-21 19:35:34 -0500

    Made prettier

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,20 +2,26 @@ const router = require("express").Router();
 let userModel = require("../models/user.model.js");
 
 router.route("/").get((req, res) => {
-    userModel.find()
-        .then(users => res.json(users))
-        .catch(err => res.status(400).json("Error: " + err));
+  userModel
+    .find()
+    .then((users) => res.json(users))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 router.route("/add").post((req, res) => {
-    const id = req.body.id;
-    const displayName = req.body.displayName;
-    const calendarLink = req.body.calendarLink;
-    const newUser = new userModel({id:id, displayName:displayName, calendarLink:calendarLink});
+  const id = req.body.id;
+  const displayName = req.body.displayName;
+  const calendarLink = req.body.calendarLink;
+  const newUser = new userModel({
+    id: id,
+    displayName: displayName,
+    calendarLink: calendarLink,
+  });
 
-    newUser.save()
-        .then(() => res.json("User added!"))
-        .catch(err => res.status(400).json("Error: " + err));
+  newUser
+    .save()
+    .then(() => res.json("User added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;

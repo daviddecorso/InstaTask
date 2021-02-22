430c0115c94c263fbdff8975ad2184409bf98a6e
lucasrossell <lrossell517@gmail.com>
2021-02-21 19:33:26 -0500

    Added basic routing functionality for users

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,12 @@ app.use(express.json());
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 const connection = mongoose.connection;
+
 connection.once("open", () => {
   console.log("Successfully connected to MongoDB.");
 });
+
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`));

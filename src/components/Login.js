8d7453e19feebcd5ba1c03dde78b3b4c2cd2df1e
David DeCorso <davidadecorso@gmail.com>
2021-02-25 16:36:24 -0500

    added login page button that goes to the auth route

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+import btnNormal from "./img/btn_google_signin_dark_normal_web.png";
+
 function Login() {
   return (
     <div>
       <p>This is the login page!</p>
+      <a href="http://localhost:5000/auth/google">
+        <img src={btnNormal} alt="Sign-in with Google." />
+      </a>
     </div>
   );
 }

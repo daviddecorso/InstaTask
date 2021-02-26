5b96a67db55733d02469d6fb30d0e2886f6b9bb7
Ethan <beauprethan@gmail.com>
2021-02-26 16:00:54 -0500

    Login page and URL validation

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,6 +1,33 @@
 import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 function Import() {
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const validate = (value) => {
+    const importBox = document.getElementById("importBox");
+    if (importBox.value.includes(".ics")) {
+      setErrorMessage("Is a valid URL");
+    } else {
+      setErrorMessage("Is not a valid URL");
+    }
+  };
+
+  const onClick = () => {
+    const importBox = document.getElementById("importBox");
+    const calendarLink = importBox.value;
+    // Alert if invalid, set box to empty
+    // Contains check
+    // console.log(calStr)
+    /*axios.post('http://localhost:5000/users/add', {
+      calendarLink: calendarLink,
+      id: id,
+      displayName: displayName
+    })
+      .then(res => console.log(res.data))*/
+  };
+
   return (
     <div>
       <div className="section">
@@ -19,13 +46,18 @@ function Import() {
                 className="input"
                 type="text"
                 placeholder="Text input"
+                id="importBox"
                 style={{ color: "black" }}
               ></input>
             </div>
           </div>
           <div className="field">
             <div className="control">
-              <button className="button is-link" id="import-submit">
+              <button
+                className="button is-link"
+                id="import-submit"
+                onClick={onClick}
+              >
                 Submit
               </button>
             </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,57 @@
 import React from "react";
 
-import btnNormal from "./img/btn_google_signin_dark_normal_web.png";
+import btnNormal from "./img/btn_google_signin_dark_normal_web2x.png";
 
 function Login() {
   return (
-    <div>
-      <p>This is the login page!</p>
-      <a href="http://localhost:5000/auth/google">
-        <img src={btnNormal} alt="Sign-in with Google." />
-      </a>
-    </div>
+    <section class="hero is-fullheight">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <div class="columns is-8 is-variable">
+            <div class="column has-text-left">
+              {/* Logo image soon */}
+              <h1 class="title is-1">InstaTask</h1>
+              <p class="is-size-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+                eligendi soluta voluptate facere molestiae consequatur.
+              </p>
+              <br />
+              <a class="button is-light">About</a>
+            </div>
+            <div class="column">
+              <br />
+              <br />
+              <a href="http://localhost:5000/auth/google">
+                <img src={btnNormal} alt="Sign-in with Google." />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    /*Not using hero
+      <section class="section is-large">
+        <div class="container has-text-centered">
+          <div class="columns is-8 is-variable">
+            <div class="column has-text-left">
+              <h1 class="title is-1">InstaTask</h1>
+              <p class="is-size-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla eligendi soluta
+							voluptate facere molestiae consequatur.</p>
+              <br />
+              <a class="button is-light">
+                About
+              </a>
+            </div>
+            <div class="column">
+            <br />
+            <br />
+              <a href="http://localhost:5000/auth/google">
+                <img src={btnNormal} alt="Sign-in with Google." />
+              </a>
+            </div>
+          </div>
+        </div>
+    </section>*/
   );
 }
 

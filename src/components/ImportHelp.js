55f64615c2e4dc58858852cc30433e392ace6d76
SeanAKemp <sean.a.kemp@gmail.com>
2021-03-14 18:05:18 -0500

    commit on main

--- src/components/ImportHelp.js
+++ src/components/ImportHelp.js
@@ -3,7 +3,11 @@ import React from "react";
 function ImportHelp() {
   return (
     <div>
-      <p>Here are instructions on how to import your calendar link:</p>
+      <p>Here are instructions on how to import your calendar link:
+        In order to import you calendar into InstaTask first navigate to Canvas and select the calendar icon on the left hand side menu. 
+        Then, below the list of calendars on the right side, select "Calendar Feed". After that you will be given a url that you can then copy 
+        to your clipboard and paste on the landing page.
+      </p>
     </div>
   );
 }

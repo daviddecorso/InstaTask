2eaf94ca513fa1bd03b91dfaad590ec0f75eb909
SeanAKemp <77809188+SeanAKemp@users.noreply.github.com>
2021-03-25 11:16:57 -0500

    added images and matched style with the about page

--- src/components/ImportHelp.js
+++ src/components/ImportHelp.js
@@ -1,15 +1,35 @@
 import React from "react";
 
+import help from "./img/importhelp.png";
+import help1 from "./img/importhelp1.png";
+import help2 from "./img/importhelp2.png";
+
 function ImportHelp() {
   return (
-    <div>
-      <h1>Here are instructions on how to import your calendar link:</h1>
-      <br></br>
-      <p>In order to import you calendar into InstaTask first navigate to Canvas and select the calendar icon on the left hand side menu. <br></br>
-         Then, below the list of calendars on the right side, select "Calendar Feed". After that you will be given a url that you <br></br>
-         can then copy to your clipboard and paste on the landing page.
-      </p>
-    </div>
+    <div className="section">
+        <div className="container">
+          <div className="columns is-8 is-variable is-centered">
+            <div className="column">
+              <h1 className="title section-title">Calendar Import Tutorial</h1>
+              <hr style={{ border: "1px solid #1659b1" }}></hr>
+              <p>
+              <img src={help1} /> <br></br>
+
+              In order to import you calendar into InstaTask first navigate to Canvas and select the calendar icon on the left hand side menu. <br></br> 
+              Then, below the list of calendars on the right side, select "Calendar Feed". <br></br>
+
+              <img src={help} /> <br></br>
+              
+              After that you will be given a url that you can then copy to your clipboard and paste on the import page when prompted.<br></br>
+
+              <img src={help2} /> <br></br>
+
+              
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
   );
 }
 
